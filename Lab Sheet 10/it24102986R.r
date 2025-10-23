3eda516a1c747f734131c73d362509c594e63ebe
IT24102986 <it24102986@my.sliit.lk>
2025-10-23 18:40:04 +0530

    Add files via upload

---
+++ Lab Sheet 10/it24102986R.r
@@ -0,0 +1,10 @@
+setwd("C:\\Users\\Vivobook\\Desktop\\it24102986")
+
+#i
+observed_counts <- c(120, 95, 85, 100)
+
+#ii
+probabilities <- c(0.25, 0.25, 0.25, 0.25)
+
+#iii
+chisq.test(x = observed_counts, p = probabilities)
